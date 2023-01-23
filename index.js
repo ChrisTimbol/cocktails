b2c8bd7c105e5a4fb723ca0a6975b071aa320c96
Chris Timbol <christimbol707@gmail.com>
2023-01-23 11:02:46 -0800

    remove ugly background and clean up code

--- index.js
+++ index.js
@@ -6,48 +6,43 @@
 // add text responses to cloudbox
 // fix github page
 
-const randomButton = document.getElementsByClassName('randomButton')[0].addEventListener("click", () => {
-    randomize()
-})
+
+const randomButton = document.getElementsByClassName('randomButton')[0].addEventListener("click", randomize)
+const searchButton = document.getElementsByClassName('searchButton')[0].addEventListener("click", searchDrink)
+
 const inputText = document.getElementsByClassName('inputText')[0]
-let searchButton = document.getElementsByClassName('searchButton')[0]
-
-
-searchButton.addEventListener("click", () => {
-    searchDrink()
-})
 inputText.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
         searchDrink();
     }
-}) 
+})
 
-const drinkContainer = document.getElementsByClassName('mainDrinkContainer')[0]
+//Container for all the drinks 
+const mainDrinkContainer = document.getElementsByClassName('mainDrinkContainer')[0]
 let isDown = false;
 let startX;
 let scrollLeft;
-drinkContainer.addEventListener('mousedown', (e) => {
+mainDrinkContainer.addEventListener('mousedown', (e) => {
     isDown = true;
-    drinkContainer.classList.add('active');
-    startX = e.pageX - drinkContainer.offsetLeft;
-    scrollLeft = drinkContainer.scrollLeft;
+    mainDrinkContainer.classList.add('active');
+    startX = e.pageX - mainDrinkContainer.offsetLeft;
+    scrollLeft = mainDrinkContainer.scrollLeft;
 });
-drinkContainer.addEventListener('mouseleave', () => {
+mainDrinkContainer.addEventListener('mouseleave', () => {
     isDown = false;
-    drinkContainer.classList.remove('active');
+    mainDrinkContainer.classList.remove('active');
 });
-drinkContainer.addEventListener('mouseup', () => {
+mainDrinkContainer.addEventListener('mouseup', () => {
     isDown = false;
-    drinkContainer.classList.remove('active');
+    mainDrinkContainer.classList.remove('active');
 });
-drinkContainer.addEventListener('mousemove', (e) => {
+mainDrinkContainer.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - drinkContainer.offsetLeft;
+    const x = e.pageX - mainDrinkContainer.offsetLeft;
     const walk = (x - startX) * 3; //scroll-fast
-    drinkContainer.scrollLeft = scrollLeft - walk;
-    //   console.log(walk);
+    mainDrinkContainer.scrollLeft = scrollLeft - walk;
 });
 
 function searchDrink() { // search by title
@@ -55,7 +50,7 @@ function searchDrink() { // search by title
         .then(res => res.json())  //res.json takes a json and parses it to produce a javascript object
         .then(json => showResults(json))
         .catch(err => console.log("Search Failed: " + err))
-    drinkContainer.innerHTML = ""; // reset drink container
+        mainDrinkContainer.innerHTML = ""; // reset drink container
 }
 
 function randomize() { // fetchs a random drink and details
@@ -63,14 +58,15 @@ function randomize() { // fetchs a random drink and details
         .then(res => res.json())  //res.json takes a json and parses it to produce a javascript object
         .then(json => showResults(json)) // gets the res.json and outputs it
         .catch(err => console.log("Random Search Failed: " + err))
-    drinkContainer.innerHTML = "";  // reset drink container
+        mainDrinkContainer.innerHTML = "";  // reset drink container
 }
 
 function showResults(result) {
     const drinkNames = result.drinks;
     console.log(drinkNames)
 
-    drinkNames.forEach((e) => {
+    drinkNames.forEach((e) => { // all the api data
+        // all the ingredients of drink
         const drinkIngr = [e.strIngredient1, e.strIngredient2, e.strIngredient3,
         e.strIngredient4, e.strIngredient5, e.strIngredient6, e.strIngredient7,
         e.strIngredient8, e.strIngredient9, e.strIngredient10, e.strIngredient11,
@@ -79,27 +75,28 @@ function showResults(result) {
         e.strMeasure4, e.strMeasure5, e.strMeasure6, e.strMeasure7, e.strMeasure8,
         e.strMeasure9, e.strMeasure10, e.strMeasure11, e.strMeasure12, e.strMeasure13,
         e.strMeasure14, e.strMeasure15]
+
+        // api elements
         let { div, thumbnail, drink, category, instructions, readButton } = declareVar();
-        div.classList = "drinkContainer"
-        drinkContainer.appendChild(div)
-        thumbnail.src = e.strDrinkThumb
-        thumbnail.classList = "thumbnail-image"
-        div.appendChild(thumbnail)
-        drink.innerText = e.strDrink
-        drink.classList = "drinkName"
-        div.appendChild(drink)
-        category.innerText = e.strCategory
-        div.appendChild(category)
-        instructions.innerText = e.strInstructions
+        
+        div.classList = "drinkDetail" // drink css 
+        mainDrinkContainer.appendChild(div) // adds drink to main drink container
+        thumbnail.src = e.strDrinkThumb // adds image
+        thumbnail.classList = "thumbnail-image" // adds class to thumbnail image
+
+        drink.innerText = e.strDrink  //  drinkName
+        drink.classList = "drinkName" 
+
+        category.innerText = e.strCategory // type of drink
+        instructions.innerText = e.strInstructions // instructions to make drink
+        instructions.classList = "instructions"
         instructions.style.display = "none"
-        instructions.style.backgroundColor = "black"
-        div.appendChild(instructions)
-        readButton.innerHTML = "Read more.."
+
+        readButton.innerHTML = "Read more.."  // read button
         readButton.classList = "readButton"
         readButton.addEventListener('click', () => {
             if (instructions.style.display === "none") {
                 instructions.style.display = "inline"
-
                 readButton.innerText = "Read less.."
             } else {
                 instructions.style.display = "none"
@@ -107,13 +104,17 @@ function showResults(result) {
             }
         })
 
+        div.appendChild(thumbnail)
+        div.appendChild(drink)
+        div.appendChild(category)
+        div.appendChild(instructions)
+
         drinkIngr.forEach((e, i) => {
             if (e !== null) {
                 let drinkIngredients = document.createElement('li')
                 drinkIngredients.innerText = (drinkMeas[i] != null) ? drinkMeas[i] + " " + e : " " + e
                 drinkIngredients.classList = "drinkIngredients"
                 drinkIngredients.style.display = "none"
-                drinkIngredients.style.backgroundColor = "black"
                 div.appendChild(drinkIngredients)
                 readButton.addEventListener('click', () => {
                     drinkIngredients.style.display === "none" ?
@@ -131,7 +132,6 @@ function showResults(result) {
         let category = document.createElement('h4')
         let instructions = document.createElement('p')
         let readButton = document.createElement('button')
-
         return { div, thumbnail, drink, category, instructions, readButton };
     }
 }
